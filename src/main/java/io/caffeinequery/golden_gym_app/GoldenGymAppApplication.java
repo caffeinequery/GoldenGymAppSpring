d782e8db2b0942eabc179be2ee0089956b2d72a5
caffeinequery <caffeinequery@gmail.com>
2024-12-20 10:08:18 -0500

    Initial commit: Spring Boot project setup.
    Generated base structure for a Spring Boot application using Spring Initializr. Includes:
    - Dependencies: JPA, MySQL Driver, and Lombok.

---
+++ src/main/java/io/caffeinequery/golden_gym_app/GoldenGymAppApplication.java
@@ -0,0 +1,13 @@
+package io.caffeinequery.golden_gym_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenGymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenGymAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/caffeinequery/golden_gym_app/GoldenGymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.caffeinequery.golden_gym_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenGymAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
